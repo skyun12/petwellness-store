7fb18caf33048eb4062c4761b64aae0afdb84753
skyun12 <84189491+skyun12@users.noreply.github.com>
2025-04-24 15:08:35 +0900

    Add files via upload

---
+++ vitamin-pet-complete/pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Vitamin Pet Page</h1>
+      <p>건강기능식품 & 반려동물 영양제 쇼핑몰 랜딩페이지입니다.</p>
+      <img src="/images/pet-banner.jpg" alt="펫 배너" width="400" />
+    </div>
+  );
+}
